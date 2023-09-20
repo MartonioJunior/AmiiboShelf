c13faaca6894a171431eb4ef91fd0301af32d5aa
Martônio Júnior <martoniojunior@hotmail.com>
2023-09-20 11:31:36 -0300

    change(AmiiboDetailViewController): Improved implementation

--- AmiiboShelf/View Controllers/AmiiboDetailViewController.swift
+++ AmiiboShelf/View Controllers/AmiiboDetailViewController.swift
@@ -34,12 +34,13 @@ class AmiiboDetailViewController: UIViewController {
         amiiboName.text = amiibo.name
         amiiboSeries.text = amiibo.amiiboSeries+" series"
         amiiboGameSeries.text = amiibo.gameSeries
-        let formatter = DateFormatter()
-        formatter.dateFormat = "yyyy-mm-dd"
-        naLaunchDate.text = amiibo.naRelease != nil ? formatter.string(from: amiibo.naRelease!) : "Not available"
-        euLaunchDate.text = amiibo.euRelease != nil ? formatter.string(from: amiibo.euRelease!) : "Not available"
-        jpLaunchDate.text = amiibo.jpRelease != nil ? formatter.string(from: amiibo.jpRelease!) : "Not available"
-        auLaunchDate.text = amiibo.auRelease != nil ? formatter.string(from: amiibo.auRelease!) : "Not available"
+    
+        let releaseDates = ReleaseDates.from(amiibo)
+        naLaunchDate.text = releaseDates.na
+        euLaunchDate.text = releaseDates.eu
+        jpLaunchDate.text = releaseDates.jp
+        auLaunchDate.text = releaseDates.au
+
         shelfButton.layer.cornerRadius = 3.0
         updateStatus(onShelf: amiibo.onShelf)
     }
@@ -51,28 +52,33 @@ class AmiiboDetailViewController: UIViewController {
         amiiboImage.image = amiibo!.image
     }
     
+    // MARK: Methods
+    @IBAction func returnButton(_ sender: UIBarButtonItem) {
+        self.dismiss(animated: true, completion: nil)
+    }
+
+    @IBAction func toggleItemOnShelf(_ sender: UIButton) {
+        guard let amiibo = amiibo else { return }
+
+        let coreData = CoreDataManager.current
+        let isOnShelf = amiibo.onShelf
+        let delegate = AmiiboDetailViewController.delegate
+
+        if !isOnShelf, coreData.addedToShelf(amiibo: amiibo) {
+            delegate?.addedToShelf(amiibo: amiibo)
+            amiibo.onShelf = true
+        } else if isOnShelf, coreData.removedFromShelf(amiibo: amiibo) {
+            delegate?.removedFromShelf(amiibo: amiibo)
+            amiibo.onShelf = false
+        }
+
+        updateStatus(onShelf: amiibo.onShelf)
+    }
+    
     func updateStatus(onShelf: Bool) {
         shelfButton.setTitle(!onShelf ? "Add to Shelf" : "Remove from Shelf", for: .normal)
         UIView.transition(with: self.view, duration: 0.5, options: .curveEaseInOut, animations: {
             self.view.backgroundColor = onShelf ? UIColor(red: 1, green: 184.0/255, blue: 51.0/255, alpha: 1) : UIColor(red: 68.0/255, green: 135.0/255, blue: 117/255.0, alpha: 1)
         }, completion: nil)
-    }
-
-    @IBAction func toggleItemOnShelf(_ sender: UIButton) {
-        guard let amiibo = amiibo else {
-            return
-        }
-        if !amiibo.onShelf, CoreDataManager.current.addedToShelf(amiibo: amiibo) {
-            AmiiboDetailViewController.delegate?.addedToShelf(amiibo: amiibo)
-            amiibo.onShelf = true
-        } else if amiibo.onShelf, CoreDataManager.current.removedFromShelf(amiibo: amiibo) {
-            AmiiboDetailViewController.delegate?.removedFromShelf(amiibo: amiibo)
-            amiibo.onShelf = false
-        }
-        updateStatus(onShelf: amiibo.onShelf)
-    }
-    
-    @IBAction func returnButton(_ sender: UIBarButtonItem) {
-        self.dismiss(animated: true, completion: nil)
     }
 }
